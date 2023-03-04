560eda42c2305b9a692d724dc37b5f46ffdcfac1
blackviva <110822720+blackviva@users.noreply.github.com>
2023-03-04 07:45:00 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -438,7 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/IrisMoviesMain),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/IrisMoviesMain'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
